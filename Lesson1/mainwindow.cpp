c54beb148997ccffd0b40c12b5eb82af0b99c111
napolnyi <napolnyi3@mail.ru>
2022-12-06 23:20:36 +0300

    v1_1

--- Lesson1/mainwindow.cpp
+++ Lesson1/mainwindow.cpp
@@ -17,4 +17,42 @@ MainWindow::~MainWindow()
 void MainWindow::on_calculateButton_clicked()
 {
 
+    ui->resultLineEdit_2->setEnabled(true);
+    ui->X2_label->setEnabled(true);
+    ui->resultLineEdit_2->clear();
+
+    double ratioA = ui->ratioA->text().toDouble();
+    double ratioB = ui->ratioB->text().toDouble();
+    double ratioC = ui->ratioC->text().toDouble();
+
+    QString x1,x2;
+
+    if (ratioA !=0)
+    {
+        double discreminate = qPow(ratioB,2)-4*ratioA*ratioC;
+        if(discreminate == 0)
+        {
+            x1 = QString::number((qSqrt(discreminate) - ratioB)/(2*ratioA));
+            ui->resultLineEdit_1->setText(x1);
+            ui->resultLineEdit_2->setEnabled(false);
+            ui->X2_label->setEnabled(false);
+        }
+        else
+        {
+            x1 = QString::number((qSqrt(discreminate) - ratioB)/(2*ratioA));
+            x2 = QString::number((-qSqrt(discreminate) - ratioB)/(2*ratioA));
+            ui->resultLineEdit_1->setText(x1);
+            ui->resultLineEdit_2->setText(x2);
+        }
+    }
+    else
+    {
+        x1 = QString::number(-ratioC/ratioB);
+        ui->resultLineEdit_1->setText(x1);
+        ui->resultLineEdit_2->setEnabled(false);
+        ui->X2_label->setEnabled(false);
+    }
+
+
+
 }
