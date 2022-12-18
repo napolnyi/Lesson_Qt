7ce52ff9d29ad679296ad8cd7dfd5a2598527d68
napolnyi <napolnyi3@mail.ru>
2022-12-18 19:24:34 +0300

    finish

--- Lesson2/Lesson2_2/Lesson2_2/mainwindow.cpp
+++ Lesson2/Lesson2_2/Lesson2_2/mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    model = new QStandardItemModel (list->size(),1,this);
+    for (int i = 0; i < model->rowCount(); ++i)
+    {
+        QModelIndex index = model->index(i,0);
+        QString str = list->at(i);
+
+        setRole(index,str);
+    }
+
+    viewMode();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +25,46 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_addButton_clicked()
+{
+
+    list->push_back("Unknown");
+    QString str = list->last();
+    QStandardItem* item = new QStandardItem (str);
+    model->appendRow(item);
+    QModelIndex index = model->index(list->size()-1,0);
+
+    setRole(index,str);
+    viewMode();
+
+}
+
+void MainWindow::on_delButton_clicked()
+{
+    model->takeRow(list->size()-1);
+    list->pop_back();
+
+    viewMode();
+
+}
+
+void MainWindow::viewMode()
+{
+    if (ui->checkBox->isChecked()) ui->listView->setViewMode(QListView::IconMode);
+    else ui->listView->setViewMode(QListView::ListMode);
+    ui->listView->setModel(model);
+}
+
+void MainWindow::setRole(QModelIndex index, QString str)
+{
+    model->setData(index,str,Qt::DisplayRole);
+    model->setData(index, str, Qt::ToolTipRole);
+    model->setData(index,QIcon("./Icons/" + str + ".png"),Qt::DecorationRole);
+}
+
+
+void MainWindow::on_checkBox_clicked()
+{
+    viewMode();
+}
--- Lesson2/Lesson2_2/Lesson2_2/mainwindow.h
+++ Lesson2/Lesson2_2/Lesson2_2/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringList>
+#include <QStandardItemModel>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_addButton_clicked();
+
+    void on_delButton_clicked();
+
+    void on_checkBox_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QStandardItemModel* model;
+    QStringList* list = new QStringList {"C++", "Python", "Java" ,"C#" ,"PHP" ,"JavaScript"};
+    void viewMode();
+    void setRole(QModelIndex index, QString str);
+
+
 };
 #endif // MAINWINDOW_H
