87a54bd837b986d1837f49c3211fadc36df605f6
napolnyi <napolnyi3@mail.ru>
2022-12-18 22:44:15 +0300

    v1.1

---
+++ Lesson2/Lesson2_3/Lesson2_3/mainwindow.cpp
@@ -0,0 +1,47 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    const int ROWCOUNT = 4;
+    const int COLUMNCOUNT = 4;
+
+
+
+    model = new QStandardItemModel(ROWCOUNT,COLUMNCOUNT,this);
+    for (int i = 0; i < model->rowCount() ; i++ )
+    {
+        for (int j=0; j < model->columnCount(); j++ )
+        {
+            QModelIndex index = model->index(i,j);
+            QString str = list.at(i)->at(j);
+            model->setData(index,str,Qt::DisplayRole);
+
+        }
+
+    }
+
+    ui->tableView->setModel(model);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_paintButton_clicked()
+{
+    QItemSelectionModel selection (model);
+    ui->tableView->setSelectionModel(&selection);
+    QModelIndex index1,index2;
+    selection.currentRowChanged(index1,index2);
+    model->setData(index1,Qt::BackgroundColorRole);
+
+
+}
---
+++ Lesson2/Lesson2_3/Lesson2_3/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QStandardItemModel>
+#include <QStringList>
+#include <QString>
+#include <QVector>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_paintButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QStandardItemModel* model;
+
+    QStringList* listColumn = new QStringList {"No", "Имя Компьютера", "IP адрес", "МАС адрес"};
+    QStringList* listRow1 = new QStringList {"1", "Имя 1", "192.168.0.1", "61-98-41-AК-CD-EА"};
+    QStringList* listRow2 = new QStringList {"2", "Имя 2", "192.168.0.2", "00:11:72:33:49:52"};
+    QStringList* listRow3 = new QStringList {"3", "Имя 3", "192.168.0.3", "61-98-58-AК-CD-EА"};
+    QVector<QStringList*> list {listColumn,listRow1,listRow2,listRow3};
+
+};
+#endif // MAINWINDOW_H
